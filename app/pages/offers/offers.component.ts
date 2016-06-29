ccc8807d565999bd2882d1c996518923c92c480b
mattpcaswell <mattpcaswell@gmail.com>
2016-06-29 15:13:00 -0400

    Added basic offer view

--- app/pages/offers/offers.component.ts
+++ app/pages/offers/offers.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OfferService } from "../../services/offers.service";
 import { Offer } from "../../services/offer";
 
 @Component({
     selector: 'offers',
-    templateUrl: 'pages/offers/offers.html'
+    templateUrl: 'pages/offers/offers.html',
+    styleUrls: ['pages/offers/offers.css'],
+    providers: [OfferService]
 })
 export class OffersComponent implements OnInit {
-    constructor() { }
+    offers: Offer[];
+
+    constructor(_offer: OfferService) { 
+        this.offers = _offer.getOffers();
+    }
+
+    onItemTap($event) {
+
+    }
 
     ngOnInit() { }
 
--- app/services/offers.service.ts
+++ app/services/offers.service.ts
@@ -9,7 +9,7 @@ export class OfferService {
             OfferID: 1,
             OfferName: "Fake Offer",
             OfferCampaignID: 1,
-            OfferContent: "This is a fake offer and this is it's fake content",
+            OfferContent: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vulputate nibh sem, rutrum faucibus magna blandit eget. Praesent id convallis risus, eu placerat arcu. Etiam luctus quis felis id blandit. Vestibulum vel turpis ante. Sed interdum ultricies augue, non feugiat eros. Sed justo dui, efficitur vitae velit ut, tristique blandit metus. Proin ut sollicitudin nulla, at dictum enim. Donec vulputate consequat finibus.",
             PromoCode: "FAKE",
             ExpirationDate: new Date("August 25, 2016")
         },
@@ -17,7 +17,71 @@ export class OfferService {
             OfferID: 2,
             OfferName: "Another Offer",
             OfferCampaignID: 1,
-            OfferContent: "This is a different offer",
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
+            PromoCode: "ANOTHER",
+            ExpirationDate: new Date("July 7, 2016")
+        },
+        {
+            OfferID: 2,
+            OfferName: "Another Offer",
+            OfferCampaignID: 1,
+            OfferContent: "Nullam volutpat mollis lacus, nec pharetra arcu sollicitudin non. Nunc id dolor vel velit mollis volutpat ut vitae felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis massa, bibendum a lobortis at, sodales vel tortor. Aliquam convallis risus eget augue dapibus, nec varius nibh fringilla. Etiam dignissim massa imperdiet augue auctor blandit. Sed blandit at nisi vitae convallis. Proin in purus suscipit, suscipit neque nec, finibus nisi.",
             PromoCode: "ANOTHER",
             ExpirationDate: new Date("July 7, 2016")
         }
