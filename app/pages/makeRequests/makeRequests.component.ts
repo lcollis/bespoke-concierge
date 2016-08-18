037f2a6426b26731c43282545cf05cdf2ea84769
mattpcaswell <mattpcaswell@gmail.com>
2016-08-18 15:04:08 -0400

    Added list of made requests to request pages

--- app/pages/makeRequests/makeRequests.component.ts
+++ app/pages/makeRequests/makeRequests.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { RequestPickerService, RequestDetails } from "../../services/requestPicker.service";
+import { TaskService } from "../../services/taskServices/task.service";
+import { UserIdService } from "../../services/userId.service";
+import { Task } from "../../services/taskServices/task";
 
 @Component({
     selector: 'makeRequests',
@@ -55,14 +58,41 @@ export class MakeRequestsComponent {
             timeLabel: "Time of reservation: ",
             hasText: true,
             textLabel: "Details: "
-        }
+        },
+        null ////null to have the list view show the made requests
     ];
 
-    constructor(private _router: Router, private _requestPickerService: RequestPickerService) {}
+    loading: boolean = true;
+    madeRequests: Task[];
+
+    constructor(private _router: Router, private _requestPickerService: RequestPickerService, private _taskService: TaskService, private _userIdService: UserIdService) {
+        this._userIdService.getUserId().then((userID: string) => {
+            this._taskService.getTasks(parseInt(userID)).subscribe((response) => {
+                this.madeRequests = response.filter((madeRequest) => {
+                    var shortDescription = madeRequest.ShortDescription;
+                    var match = false;
+                    this.requests.forEach((request) => {
+                        if (request) {
+                            if (request.title === shortDescription) {
+                                match = true;
+                            }
+                        }
+                    });
+                    return match;
+                });
+
+                this.loading = false;
+            }, (error) => {
+                console.log(error);
+            });
+        });
+    }
 
     onItemTap(args) {
-        var selectedRequest: RequestDetails = this.requests[args.index];
-        this._requestPickerService.requestDetails = selectedRequest;
-        this._router.navigate(["/GuestScreen/RequestDetails"]);
+        if (args.index < this.requests.length - 1) {
+            var selectedRequest: RequestDetails = this.requests[args.index];
+            this._requestPickerService.requestDetails = selectedRequest;
+            this._router.navigate(["/GuestScreen/RequestDetails"]);
+        }
     }
 }
